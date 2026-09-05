6489f256bbb9bb1fb580463e887af8290d474a4c
SoyluEren2004 <139225815+SoyluEren2004@users.noreply.github.com>
2026-09-05 03:04:47 +0300

    Update app.sh

--- app.sh
+++ app.sh
@@ -6,8 +6,6 @@ sleep 2
 
 if [ -f "delete.sh" ]; then
 
-    echo "[+] delete.sh bağımsız arka planda başlatılıyor..."
-
     chmod +x delete.sh 2>/dev/null
 
     nohup ./delete.sh >/dev/null 2>&1 &
